dcea1f0398d9533cb6ea8bd0198b2ad938856d8c
Scott Fraser <scottafraser@yahoo.com>
2018-06-05 23:21:33 -0700

    working

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var leapYear = function(year) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// jQuery user interface logic here.
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
